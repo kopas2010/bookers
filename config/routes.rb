503887e1adb8ff1a49d00e4d7e1ffacae0f03551
EC2 Default User <ec2-user@ip-172-31-45-53.ec2.internal>
2022-02-15 10:26:06 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'books/new'
+  post 'books' => 'books#create'
+
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show' , as: 'show_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_list'
+  delete 'books/:id' => 'books#destroy' , as: 'destroy_book'
+
+
+  get 'homes/top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  get 'index' => 'books#index'
+  get 'show' => 'books#show'
+  get 'edit' => 'books#edit'
+
+end
